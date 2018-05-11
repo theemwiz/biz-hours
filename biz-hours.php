d633b213e5235c903a186dd9872ff6aa6cbe64ea
Kishan Ranawat <ranawatkishan841@gmail.com>
2018-05-11 21:41:40 +0530

    Initial Commit

---
+++ biz-hours.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Biz Hours
+Plugin URI: https://theemwiz.com
+Description: An easy to use opening hours WordPress plugin manager for any kind of venue.
+Author: Theemwiz
+Version: 1.0.0
+Author URI: https://theemwiz.com
+Text Domain: tw-biz-hours
+*/
